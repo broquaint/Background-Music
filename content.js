ff5dd715f202191c6c13d4898d5b4aa9ae177673
Dan Brook <dbrook@venda.com>
2010-06-08 13:43:37 +0100

    A small hack to get Soundcloud tracks playing.
    This is just a proof of concept which will play the first track of a
    Soundcloud track listing in BM.

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
-$("a[href$='mp3']").click(function() {
+$("a[href$='mp3'], a.download").click(function() {
   chrome.extension.sendRequest({audioSrc: this.href});
   // TODO - Add notification the song has been queued to play.
   return false;
+});
+
+$(function() {
+  var src = document.getElementsByTagName('script');
+      js  = src[src.length - 1].innerText.match(/db:({.*})\}/),
+      db  = JSON.parse(js[1]),
+      trck= $('#main-content-inner div.player:first').attr('data-sc-track');
+  chrome.extension.sendRequest({audioSrc: db.tracks[trck].streamUrl});
 });
